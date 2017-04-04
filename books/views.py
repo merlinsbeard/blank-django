92b878502c06b81a2d8330960ba45ee4822dbb9c
BJ Paat <brpaat@ayannah.com>
2017-04-04 15:12:18 +0800

    Update book.views to contain api views

--- books/views.py
+++ books/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Book, Author
+from rest_framework import generics
+from .serializers import BookSerializer, AuthorSerializer
 
-# Create your views here.
+
+class BookList(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class AuthorList(generics.ListAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class AuthorDetail(generics.RetrieveAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
