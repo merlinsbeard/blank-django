cbbb7c2d6e7e5ebf1ce6a78006e44d7d23de151e
BJ Paat <brpaat@ayannah.com>
2017-04-04 16:14:50 +0800

    Update foreignkey to add related_name

--- books/models.py
+++ books/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Book(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
-    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    author = models.ForeignKey('Author',
+                on_delete=models.CASCADE, related_name='book')
 
     def __str__(self):
         return self.name
