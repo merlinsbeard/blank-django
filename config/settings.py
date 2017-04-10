e9075178a97671bf79518c566c9774d655d5dc8b
BJ Paat <brpaat@ayannah.com>
2017-04-10 10:13:59 +0800

    Update postgres to use localhost

--- config/settings.py
+++ config/settings.py
@@ -90,7 +90,8 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME', 'cookie'),
         'USER': os.getenv('DB_USER', 'postgres'),
         'PASSWORD': os.getenv('DB_PASSWORD', 'postgres'),
-        'HOST': os.getenv('DB_HOST', 'pg-service'),
+ #       'HOST': os.getenv('DB_HOST', 'pg-service'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': '5432',
     }
 }
